34bbdc3e4ae914d93f5c9593a8b64e5e71645c25
mrdimsum99 <deonkidjo_18@hotmail.com>
2018-03-19 11:41:10 +0100

    Verhaaltje script aangemaakt

---
+++ Verhaalscript.py
@@ -0,0 +1,14 @@
+file1 = open("text1.txt", "r")
+file2 = open("text2.txt", "r")
+file3 = open("text3.txt", "r")
+file4 = open("text4.txt", "r")
+
+txt1 = file1.read()
+txt2 = file2.read()
+txt3 = file3.read()
+txt4 = file4.read()
+
+print(txt1)
+print(txt2)
+print(txt3)
+print(txt4)
